b5eafc47bc8d756da85d1122fd6bb77fcf133908
cornedtiger <122634501+cornedtiger@users.noreply.github.com>
2023-01-13 23:00:06 +0500

    Add files via upload

--- umer.py
+++ umer.py
@@ -1,17 +1,8 @@
-# serialize.py
+# deserialize.py
 import yaml
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person needs to be present in the scope
+with open('person.yml', 'r') as input_file:
+    person = yaml.load(input_file, Loader = yaml.Loader)
 
-    def __str__(self):
-        return f'<Person: {self.name} - {self.age}>'
-
-    def __repr__(self):
-        return str(self)
-
-person = Person('Dhruv', 24)
-with open('person.yml', 'w') as output_file:
-    yaml.dump(person, output_file)
+print(person)
